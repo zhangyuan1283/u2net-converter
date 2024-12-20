648cb9cadbf85dfa4d9729fe705fa2f07c4e5a07
Zhang Yuan <zhang_yuan@franknetwork.co>
2024-12-20 10:56:29 +0800

    Modify script to also output number of output tensors

--- model_dimensions.py
+++ model_dimensions.py
@@ -1,7 +1,17 @@
 import tensorflow as tf
 
-interpreter = tf.lite.Interpreter(model_path="./content/u2net.tflite")
+# Load the TensorFlow Lite model
+interpreter = tf.lite.Interpreter(model_path="./content/model_float32.tflite")
 interpreter.allocate_tensors()
 
+# Get all tensor details
+tensor_details = interpreter.get_output_details()
+total_tensors = len(tensor_details)
+
+# Print the total number of tensors
+print("Total number of tensors:", total_tensors)
+
+# Print input details
 input_details = interpreter.get_input_details()
 print("Input Details:", input_details)
+
